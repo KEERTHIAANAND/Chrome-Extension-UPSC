af0c40c154c4158160e44f4e5d378352a1b47376
KEERTHIAANAND <keerthiaanand77@gmail.com>
2025-12-11 15:14:18 +0530

    add index.js to export core components for the extension

---
+++ UPSC-Extension/src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Sidebar } from './Sidebar';
+export { default as Header } from './Header';
+export { default as Journey } from './Journey';
+export { default as StatsCards } from './StatsCards';
+export { default as DailyMissions } from './DailyMissions';
+export { default as StreakTimer } from './StreakTimer';
+export { default as Bookmarks } from './Bookmarks';
+export { default as QuickNotes } from './QuickNotes';
